02683373f63130ad88ee2effffd46d32d32bf4d8
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2021-02-09 18:57:01 -0600

    Finished the left sidebar

---
+++ linkedin-clone/src/Sidebar.js
@@ -0,0 +1,49 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  )
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1546188994-07c34f6e5e1b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8ZnV0dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Daniel Peter-Salawu</h2>
+        <h4>daniel.petersalawu@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">3,000</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,353</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('softwareengineering')}
+        {recentItem('programming')}
+        {recentItem('developer')}
+        {recentItem('frontend')}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
