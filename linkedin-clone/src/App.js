02b1b188ef104ef52f71888ec83bf20ed7567fc0
DPS7991 <66211631+DPS7991@users.noreply.github.com>
2021-02-22 22:59:43 -0600

    Started working on the login page

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,19 +1,27 @@
 import "./App.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widget */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* Widget */}
+        </div>
+      )}
     </div>
   );
 }
